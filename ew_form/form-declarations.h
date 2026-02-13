3314952a8bf72906fdba61fc582c6e57406ca7ca
AnaIPereira <ana.pereira98@icloud.com>
2026-02-13 15:04:52 +0000

    ch

--- ew_form/form-declarations.h
+++ ew_form/form-declarations.h
@@ -32,15 +32,13 @@ Vector momen,momen1;
 
 *Coupling constants and gauge parameters
 Symbol gaug, xia, xiw, xiz;
-Symbol ufoGC58,ufoGC5,ufoGC54,ufoGC107, ufoGC90,ufoGC56,ufoGC53,ufoGC52,ufoGC69,ufoGC3,ufoGC55,ufoGC76,ufoGC80,ufoGC85,
-ufoGC81,iufo11, iufo13,ufoGC4,ufoGC93,ufoGC68,ufoGC119,ufoGC59,ufoGC120,ufoGC60,
-ufoGC121,ufoGC61,ufoGC122,ufoGC62,ufoGC123,ufoGC63,ufoGC124,ufoGC64,ufoGC125,ufoGC65,
-ufoGC126,ufoGC66,ufoGC127,ufoGC67,ufoGC57,ufoGC92,ufoGC94,ufoGC95,ufoGC15,ufoGC24,ufoGC18,
-ufoGC27,ufoGC21,ufoGC30,ufoGC86;
+
+AutoDeclare Index iufo;
+AutoDeclare Symbol ufoGC;
 
 *indices
 Symbol mu,spin;
-Indices ind,ind1,ind2,ind3, ind4,nu,i,j,iufo3,iufo4,iufo15;
+Indices ind,ind1,ind2,ind3, ind4,nu,i,j,iufo3,iufo13,iufo4,iufo15;
 
 *gamma matrices
 Function auxGamma, auxPL, auxPR,auxSlash;
